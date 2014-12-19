c480f0c4a0a861a73eace71de52d11d24a2bce54
Philipp Sackl <philipp@mozilla.com>
2014-12-19 13:11:49 +0100

    structure looks good, doesn't work yet though

--- src/docify.js
+++ src/docify.js
@@ -1,13 +1,35 @@
+var attemptSections = 50;
+var attemptSketches = 50;
+
+var cat = String.concat;
 
 
-$.ajax({
-  url: "./content/test.txt", 
-  success: function(res) {
-    console.log(String.split(res, "\n"));
-    _.each( String.split(res, "\n"), function(item) {
-      $('body').append( $('<canvas data-processing-sources="content/'+ item +'"></canvas>') );
+function parseContent(basePath) {
+  var allContent = [];
+  for (var i=0; i<attemptSections; i++) {
+    // Attempt to load the headline file of a section
+    loadText(cat(basePath, "/", i, "/headline.txt"), function(headlineResponse) {
+      var section = {headline: headlineResponse, text: "", sketches: []};
+      // Attempt to load text file
+      loadText(cat(basePath, "/text.txt"), function(textResponse) {
+        section.text = textResponse;
+      });
+      // Attempt to load sketches
+      for (var j=0; j<attemptSketches; j++) {
+        loadText(cat(basePath, "/", i, "/", j, ".pde"), function(pdeResponse) {
+          section.sketches[j] = cat(basePath, "/", i, "/", j, ".pde");
+        });
+      }
+      allContent[i] = section;
     });
-  },
-  dataType: "text"
-});
+  }
+  return allContent;
+}
 
+function loadText(path, success) {
+  $.ajax({
+    url: path, 
+    success: success,
+    dataType: "text"
+  });
+}
