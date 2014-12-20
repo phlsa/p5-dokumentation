67247cb0395656b5c3cee8fc9e130ed164b55645
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2014-12-20 19:51:42 +0100

    new Data

--- src/dummyData.js
+++ src/dummyData.js
@@ -4,7 +4,7 @@ var data = [
     text: "Bacon ipsum dolor amet pork loin alcatra sausage, corned beef meatball picanha beef ribs swine fatback. Sausage pig beef tongue picanha corned beef shoulder landjaeger porchetta. Cupim ham hock ground round chicken short loin, turkey beef jerky shankle tongue biltong rump venison pancetta. Fatback boudin spare ribs turkey alcatra, filet mignon pork. Fatback ribeye ball tip cupim. Short loin sausage tri-tip flank, ground round filet mignon brisket t-bone hamburger salami. Jerky venison doner, spare ribs beef ribs ribeye ground round. Boudin ham tenderloin, chicken meatloaf tongue porchetta pig ham hock fatback alcatra sirloin cupim. Landjaeger boudin frankfurter capicola tenderloin brisket chicken bacon andouille.Picanha andouille pancetta, filet mignon drumstick beef leberkas kielbasa chuck. Ham chuck turducken ground round shank. Capicola ground round meatball, pork belly t-bone andouille turkey. Short loin turkey spare ribs leberkas, meatloaf shoulder pork bacon pastrami turducken ham cupim. Pork belly drumstick prosciutto ground round bacon frankfurter tongue swine tail brisket strip steak.",
     sketches: [
       {url: "./content/1/1.pde", text: "Beschreibung für Sketch eins", isPDE: true},
-      {url: "./content/1/2.pde", text: "Beschreibung für Sketch zwei", isPDE: false},
+      {url: "./content/1/2.png", text: "Beschreibung für Sketch zwei", isPDE: false},
       {url: "./content/1/3.pde", text: "Beschreibung für Sketch drei", isPDE: true}
     ]
   },
@@ -12,9 +12,9 @@ var data = [
     title: "Zweites Thema",
     text: "Hier kommt der Beschreibungstext hin",
     sketches: [
-      {url: "./content/2/1.pde", text: "Beschreibung für Sketch eins", isPDE: false},
+      {url: "./content/2/1.png", text: "Beschreibung für Sketch eins", isPDE: false},
       {url: "./content/2/2.pde", text: "Beschreibung für Sketch zwei", isPDE: true},
-      {url: "./content/2/3.pde", text: "Beschreibung für Sketch drei", isPDE: false}
+      {url: "./content/2/3.png", text: "Beschreibung für Sketch drei", isPDE: false}
     ]
   }
 ]
