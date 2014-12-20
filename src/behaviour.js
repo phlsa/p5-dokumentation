d828acbb490d77d322b484e4872a17c29dff63e2
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2014-12-20 17:19:36 +0100

    behaviour changes

--- src/behaviour.js
+++ src/behaviour.js
@@ -1,16 +1,42 @@
+
+
 window.onload = function() {
+
+    var selectedChapter = 0;
     
     $(".menu-entry").first().addClass("selected")
     $(".chapter").first().fadeIn();
 
-    $(".menu-entry").click(function() {
-        console.log("click");
+
+    $( ".menu-entry" ).each(function( index ) {
+        var that = this;
+        $(this).click( function() {
+            var dir = 1;
+            if (index > selectedChapter) {
+                dir = -1;
+            }
+
+            $( ".menu-entry:nth-child("+(selectedChapter+1)+")" ).removeClass("selected");
+            $( ".chapter:nth-child("+(selectedChapter+1)+")" ).fadeOut({ 
+                progress: function(obj, p, r) {
+                    $(obj.elem).css("top", 30+dir*(p)*50);
+                }
+            });
+            selectedChapter = index;
+            $( ".menu-entry:nth-child("+(selectedChapter+1)+")" ).addClass("selected");
+            $( ".chapter:nth-child("+(selectedChapter+1)+")" ).fadeIn({ 
+                progress:function(obj, p, r) {
+                    $(obj.elem).css("top", 30-dir*(1-p)*50);
+                }
+            });
+        });
+    });
+/*
+    $(".menu-entry").click(function(e) {
+        console.log(e);
 
     });
-
+*/
 
 };
 
-function show(chapter) {
-
-}
