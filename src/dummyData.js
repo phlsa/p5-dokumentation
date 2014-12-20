8ff69c62b4dac450f8371dbc10b60aad7ae90006
Philipp Sackl <philipp@mozilla.com>
2014-12-20 12:40:19 +0100

    add dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,20 @@
+var data = [
+  {
+    title: "Erstes Thema",
+    text: "Hier kommt der Beschreibungstext hin",
+    sketches: [
+      {url: "./content/1/1.pde", text: "Beschreibung für Sketch eins"},
+      {url: "./content/1/2.pde", text: "Beschreibung für Sketch zwei"},
+      {url: "./content/1/3.pde", text: "Beschreibung für Sketch drei"}
+    ]
+  },
+  {
+    title: "Zweites Thema",
+    text: "Hier kommt der Beschreibungstext hin",
+    sketches: [
+      {url: "./content/2/1.pde", text: "Beschreibung für Sketch eins"},
+      {url: "./content/2/2.pde", text: "Beschreibung für Sketch zwei"},
+      {url: "./content/2/3.pde", text: "Beschreibung für Sketch drei"}
+    ]
+  }
+]
