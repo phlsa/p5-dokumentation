6448269f7ed4db4fce2693d5b8f4fdf4bd120be0
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2014-12-20 15:58:58 +0100

    adjust styling and behaviour

---
+++ src/behaviour.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    
+    $(".menu-entry").first().addClass("selected")
+    $(".chapter").first().fadeIn();
+
+    $(".menu-entry").click(function() {
+        console.log("click");
+
+    });
+
+
+};
+
+function show(chapter) {
+
+}
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,7 +1,7 @@
 var data = [
   {
     title: "Erstes Thema",
-    text: "Hier kommt der Beschreibungstext hin",
+    text: "Bacon ipsum dolor amet pork loin alcatra sausage, corned beef meatball picanha beef ribs swine fatback. Sausage pig beef tongue picanha corned beef shoulder landjaeger porchetta. Cupim ham hock ground round chicken short loin, turkey beef jerky shankle tongue biltong rump venison pancetta. Fatback boudin spare ribs turkey alcatra, filet mignon pork. Fatback ribeye ball tip cupim. Short loin sausage tri-tip flank, ground round filet mignon brisket t-bone hamburger salami. Jerky venison doner, spare ribs beef ribs ribeye ground round. Boudin ham tenderloin, chicken meatloaf tongue porchetta pig ham hock fatback alcatra sirloin cupim. Landjaeger boudin frankfurter capicola tenderloin brisket chicken bacon andouille.Picanha andouille pancetta, filet mignon drumstick beef leberkas kielbasa chuck. Ham chuck turducken ground round shank. Capicola ground round meatball, pork belly t-bone andouille turkey. Short loin turkey spare ribs leberkas, meatloaf shoulder pork bacon pastrami turducken ham cupim. Pork belly drumstick prosciutto ground round bacon frankfurter tongue swine tail brisket strip steak.",
     sketches: [
       {url: "./content/1/1.pde", text: "Beschreibung für Sketch eins"},
       {url: "./content/1/2.pde", text: "Beschreibung für Sketch zwei"},
