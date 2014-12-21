6ce21abb3ec88862bc99969b71d561bb61b49ad2
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2014-12-21 11:31:39 +0100

    Tried to reload processing sketch
    Is still not working perfectly, seems to be a bug in
    Processing.getInstanceById()

--- src/behaviour.js
+++ src/behaviour.js
@@ -1,42 +1,52 @@
-
-
 window.onload = function() {
 
     var selectedChapter = 0;
-    
+
     $(".menu-entry").first().addClass("selected")
     $(".chapter").first().fadeIn();
 
 
-    $( ".menu-entry" ).each(function( index ) {
+    $(".menu-entry").each(function(index) {
         var that = this;
-        $(this).click( function() {
+        $(this).click(function() {
             var dir = 1;
             if (index > selectedChapter) {
                 dir = -1;
             }
 
-            $( ".menu-entry:nth-child("+(selectedChapter+1)+")" ).removeClass("selected");
-            $( ".chapter:nth-child("+(selectedChapter+1)+")" ).fadeOut({ 
+            $(".menu-entry:nth-child(" + (selectedChapter + 1) + ")").removeClass("selected");
+            $(".chapter:nth-child(" + (selectedChapter + 1) + ")").fadeOut({
                 progress: function(obj, p, r) {
-                    $(obj.elem).css("top", 30+dir*(p)*50);
+                    $(obj.elem).css("top", 30 + dir * (p) * 50);
                 }
             });
             selectedChapter = index;
-            $( ".menu-entry:nth-child("+(selectedChapter+1)+")" ).addClass("selected");
-            $( ".chapter:nth-child("+(selectedChapter+1)+")" ).fadeIn({ 
-                progress:function(obj, p, r) {
-                    $(obj.elem).css("top", 30-dir*(1-p)*50);
+            $(".menu-entry:nth-child(" + (selectedChapter + 1) + ")").addClass("selected");
+            $(".chapter:nth-child(" + (selectedChapter + 1) + ")").fadeIn({
+                progress: function(obj, p, r) {
+                    $(obj.elem).css("top", 30 - dir * (1 - p) * 50);
                 }
             });
         });
     });
-/*
-    $(".menu-entry").click(function(e) {
-        console.log(e);
-
-    });
-*/
 
 };
+
+function resetSketch(resetButton) {
+    var sketchUrl = resetButton.dataset.url;
+    var res = $("body").find("[data-processing-sources='" + sketchUrl + "']");
+    var canvas = res[0];
+
+    // TODO:getInstanceByID seems to be buggy
+    var instance = Processing.getInstanceById(canvas.id);
+    instance.exit();
+    //var context = canvas.getContext('2d');
+    //context.clearRect(0, 0, canvas.width, canvas.height);
+
+    Processing.loadSketchFromSources(canvas, [sketchUrl]);    
+}
+
+
+
+
 
